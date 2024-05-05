d6b8a524a9c0d8936088b3588662251a4a46f0e8
branch10480 <toshiharu.imaeda@gmail.com>
2024-05-05 19:25:13 +0900

    first commit

---
+++ src/main/java/com/example/samplewebfluxapp/SampleWebFluxAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.samplewebfluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebFluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebFluxAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/samplewebfluxapp/controller/HelloController.java
@@ -0,0 +1,51 @@
+package com.example.samplewebfluxapp.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+public class HelloController {
+    
+    private final WebClient webClient;
+    
+    public HelloController(WebClient.Builder builder) {
+        super();
+        this.webClient = builder.baseUrl("jsonplaceholder.typicode.com").build();
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Spring WebFlux!!";
+    }
+    
+    @GetMapping("/flux")
+    public Flux<String> fluxSample() {
+        return Flux.just("Hello, ", "!!");
+    }
+    
+    @GetMapping("/web/{id}")
+    public Mono<String> web(@PathVariable int id) {
+        return this.webClient.get()
+            .uri("/posts/" + id)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+    
+    @GetMapping("/web")
+    public Mono<String> web2() {
+        return this.webClient.get()
+            .uri("/posts")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(String.class);
+    }
+    
+}
